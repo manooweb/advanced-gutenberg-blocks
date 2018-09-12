95a33f775e1a14a4078b3195f688149c540cf112
Emmanuel Hesry <ehesry@gmail.com>
2018-09-12 13:52:06 +0200

    build

--- src/blocks/summary/list.js
+++ src/blocks/summary/list.js
@@ -12,7 +12,8 @@ export default class List extends Component {
   // Inspired by packages/editor/src/components/document-outline/index.js in Gutenberg project
   computeOutlineHeadings = ( blocks = [], path = [] ) => {
     return flatMap( blocks, ( block = {} ) => {
-      if ( block.name === 'core/heading' ) {
+
+      if ( block.name === 'core/heading' && block.attributes.content.length > 0 ) {
 
         // Define anchor slug
         let slug = block.attributes.content[0]
@@ -20,7 +21,6 @@ export default class List extends Component {
           .replace( /[\s#]/g, '-' )                      // turn spaces to dashes
           .replace( /-$/, "" )                          // remove eventual last dash
           .toLowerCase()                               // lowercase it
-
 
         // Update the title block with the anchor slug ID
         this.props.updateBlockAttributes( block.clientId, { anchor: slug } )
